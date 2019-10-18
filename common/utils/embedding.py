9a02d1ce98a19d55c31a45b1c35241691391d069
Anish Acharya <anishacharya@utexas.edu>
2019-10-18 03:35:59 -0500

    setting up seq2seq

--- common/utils/embedding.py
+++ common/utils/embedding.py
@@ -14,8 +14,6 @@ class WordEmbedding:
         self.emb_dim = 0
         self.ix2embed = self.load_and_index_word_embedding()
 
-
-
     def load_and_index_word_embedding(self) -> Dict:
         """
         Read a GloVe txt file.we return dictionary
