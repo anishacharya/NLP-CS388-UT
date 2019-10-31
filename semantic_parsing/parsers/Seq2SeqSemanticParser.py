bc31f48edb621fed9198fb0d93218031df3442b1
Anish Acharya <anishacharya@utexas.edu>
2019-10-31 04:02:40 -0500

    +

--- semantic_parsing/parsers/Seq2SeqSemanticParser.py
+++ semantic_parsing/parsers/Seq2SeqSemanticParser.py
@@ -1,13 +1,28 @@
 from semantic_parsing.data_utils.definitions import Example, Derivation
+import semantic_parsing.semantic_parser_config as parser_conf
+from common.utils.indexer import Indexer
 from typing import List
+import torch.nn as nn
 
 
 class Seq2SeqSemanticParser(object):
-    def __init__(self, training_data: List[Example]):
-        self.training_data = training_data
+    def __init__(self, training_data: List[Example],
+                 dev_data: List[Example],
+                 input_ix: Indexer,
+                 output_ix: Indexer):
+        self.train_data = training_data
+        self.dev_data = dev_data
 
     def train(self):
-        raise NotImplementedError
+        acc = 0.0
+        last_epoch_acc = 0.0
+        lr = parser_conf.initial_lr
+        lr_decay = parser_conf.lr_decay
+        weight_decay = parser_conf.weight_decay
+
+        epochs = parser_conf.epochs
+        batch_size = parser_conf.batch_size
+        loss_function = nn.BCELoss()
 
     def decode(self, test_data: List[Example]) -> List[List[Derivation]]:
         # Implement the inference here
--- semantic_parsing/semantic_parser_config.py
+++ semantic_parsing/semantic_parser_config.py
@@ -4,8 +4,22 @@ output_path = 'geo_test_output.tsv'
 
 parser = 'NN'
 
-model = 'FFNN'  # RNN, FFNN
 run_on_test_flag = True
 run_on_manual_flag = True
 seq_max_len = 60  # also can be computed more systematically looking at length distribution in corpus
 model_path = './model.pt'
+
+epochs = 30
+batch_size = 64
+lr_schedule = 'None'  # None / CLR / CALR
+optimizer = 'adam'  # adagrad
+initial_lr = 0.01
+lr_decay = 0.1
+weight_decay = 1e-4
+dropout = 0.2
+
+# Stacked RNN units
+no_of_rec_units = 2
+# inside RNN unit
+hidden_size = 100
+rnn_dropout = 0.05
